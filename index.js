f8e050b47b8320892fa58b809e2eb9862322a6d3
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-09-22 20:52:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict"
+// Функция сравнивает числа
+
+// function calcSum(from,to) {
+//     let sum = 0;
+//     while (true) {
+//         const number = +prompt(`Please enter your number`);
+//
+//         if (!isNaN(number))
+//
+//             if (number >= from && number <= to) {
+//                 sum = sum + number
+//             } else {
+//                 return sum;
+//             }
+//         ;
+//     }
+// }
+// const result=calcSum(4,20)
+// console.log(result)
+
+// Функция приводит строку к Верхний регистр первая буква, с маленькой все остальные
+
+// function capitalazeWord(word){
+//     return`${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
+// }
+
+// Функция очищает строку от пробелов
+// function middleTrim(str){
+//     let result=``;
+//     for( let index=0;index<str.length;index++){
+//         const char=str[index];
+//         if(char===` `){
+//             continue;
+//         }
+//         result=`${result}${char}`
+//         console.log(str[index]);
+//
+//     }
+//     return result;
+// }
